61b0581ce60ec5292ac30f476e1324e4aba2d6b0
joshgibs <joshgibson82@gmail.com>
2018-09-03 16:19:37 -0400

    Through video 9

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,7 +11,9 @@ from PIL import Image
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+
     return render_template('home.html', posts=posts)
 
 
@@ -23,37 +25,50 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
+
         return redirect(url_for('home'))
+
     form = RegistrationForm()
+
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
+
         return redirect(url_for('login'))
+
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+
         return redirect(url_for('home'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+
             return redirect(next_page) if next_page else redirect(url_for('home'))
+
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
+
     return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/logout")
 def logout():
     logout_user()
+
     return redirect(url_for('home'))
 
 
@@ -75,7 +90,9 @@ def save_picture(form_picture):
 @login_required
 def account():
     form = UpdateAccountForm()
+
     if form.validate_on_submit():
+
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
@@ -84,11 +101,14 @@ def account():
         current_user.email = form.email.data
         db.session.commit()
         flash('Your account has been updated.', 'success')
+
         return redirect(url_for('account'))
+
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+
     return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 
@@ -125,6 +145,7 @@ def update_post(post_id):
 
     if post.author != current_user:
         abort(403)
+
     form = PostForm()
 
     if form.validate_on_submit():
@@ -158,3 +179,14 @@ def delete_post(post_id):
     flash('Your post has been deleted!', 'success')
 
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+
+    return render_template('user_posts.html', posts=posts, user=user)
